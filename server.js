a300f1fe45fffeb15dba6a82b851f42208a2937c
Vitor Gomes <vct@outlook.com>
2026-07-21 04:54:08 +0000

    Improviments to Google SEO

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const TYPES = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
   ".woff2": "font/woff2",
   ".png": "image/png",
   ".svg": "image/svg+xml",
